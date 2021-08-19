5b347c36179243dc32607a57175b40b38a047a2b
Leonel-Padilla <axcel.sanchez1@ujcv.edu.hn>
2021-08-19 02:30:38 -0600

    Spinner Funcionando

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/Maquinaria/Buscar_Maquinaria_Activity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/Maquinaria/Buscar_Maquinaria_Activity.kt
@@ -5,9 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.MenuPrincipal.MenuActivity
 import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.R
+import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.RestEngine
 import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.Toolbar.MyToolbar
+import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.entities.MaquinariaDataCollectionItem
+import kotlinx.android.synthetic.main.activity_buscar_maquinaria.*
+import kotlinx.android.synthetic.main.activity_registro_maquinaria.*
+import kotlinx.android.synthetic.main.activity_registro_maquinaria.txtMaquinariaId
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Buscar_Maquinaria_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +25,61 @@ class Buscar_Maquinaria_Activity : AppCompatActivity() {
         setContentView(R.layout.activity_buscar_maquinaria)
 
         MyToolbar().show(this,"Buscar Maquinaria", false)
+        btnBuscarMaqui2.setOnClickListener{ callServiceGetMaquinaria() }
+        btnEliminarMaqui.setOnClickListener { callServiceDeleteMaquinaria() }
+    }
+
+
+    //GET
+    private fun callServiceGetMaquinaria() {
+        val maquinariaService:MaquinariaService = RestEngine.buildService().create(MaquinariaService::class.java)
+        var result: Call<MaquinariaDataCollectionItem> = maquinariaService.getMaquinariaById(txtMostrarMaquinariaId.text.toString().toLong())
+
+        result.enqueue(object : Callback<MaquinariaDataCollectionItem> {
+            override fun onFailure(call: Call<MaquinariaDataCollectionItem>, t: Throwable) {
+                Toast.makeText(this@Buscar_Maquinaria_Activity,"Error", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                call: Call<MaquinariaDataCollectionItem>,
+                response: Response<MaquinariaDataCollectionItem>
+            ) {
+                txtMostrarMaquinariaId.setText(response.body()!!.maquinaId.toString())
+                txvMostrarNombreMarca.setText(response.body()!!.marca)
+                txvMostrarFarbicaId.setText(response.body()!!.fabricaId.toString())
+                txvMostrarHorasUsoMaqui.setText(response.body()!!.horasUso.toString())
+                txvMostrarTipoMaquina.setText(response.body()!!.tipoMaquina)
+                Toast.makeText(this@Buscar_Maquinaria_Activity,"Se encontró la maquina con Id"+response.body()!!.maquinaId, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    //DELETE
+
+    private fun callServiceDeleteMaquinaria() {
+        val maquinariaService:MaquinariaService = RestEngine.buildService().create(MaquinariaService::class.java)
+        var result: Call<ResponseBody> = maquinariaService.deleteMaquinaria(txtMostrarMaquinariaId.text.toString().toLong())
+
+        result.enqueue(object :  Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(this@Buscar_Maquinaria_Activity,"Error",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                if (response.isSuccessful) {
+                    Toast.makeText(this@Buscar_Maquinaria_Activity,"Maquinaria Eliminada",Toast.LENGTH_LONG).show()
+                }
+                else if (response.code() == 401){
+                    Toast.makeText(this@Buscar_Maquinaria_Activity,"Sesion expirada",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this@Buscar_Maquinaria_Activity,"Fallo al traer el item",Toast.LENGTH_LONG).show()
+                }
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/Maquinaria/Registro_Maquinaria_Activity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/Maquinaria/Registro_Maquinaria_Activity.kt
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.Factura.Buscar_Factura_Activity
-import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.Factura.Registro_Factura_Activity
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.google.gson.Gson
+import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.Fabrica.FabricaService
 import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.MenuPrincipal.MenuActivity
 import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.R
+import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.RestEngine
 import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.Toolbar.MyToolbar
+import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.entities.FabricaDataCollectionItem
+import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.entities.MaquinariaDataCollectionItem
+import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.entities.RestApiError
+import kotlinx.android.synthetic.main.activity_registro_maquinaria.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Registro_Maquinaria_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +27,183 @@ class Registro_Maquinaria_Activity : AppCompatActivity() {
         setContentView(R.layout.activity_registro_maquinaria)
 
         MyToolbar().show(this,"Registrar Maquinaria", false)
+
+        callServiceGetMaquinarias()
+        callServicesGetFabricas()
+        btnBuscarMaqui.setOnClickListener { callServiceGetMaquinaria() }
+        btnRegisMaqui.setOnClickListener { callServicePostMaquinaria() }
+        btnActuMaqui.setOnClickListener { callServicePutMaquinaria() }
     }
+
+    //-----
+
+    //GETFABRICAS
+    private fun callServicesGetFabricas(){
+        val fabricas = ArrayList<String>()
+        val fabricaService:FabricaService = RestEngine.buildService().create(FabricaService::class.java)
+        var result: Call<List<FabricaDataCollectionItem>> = fabricaService.listFabricas()
+
+        result.enqueue(object :  Callback<List<FabricaDataCollectionItem>> {
+            override fun onFailure(call: Call<List<FabricaDataCollectionItem>>, t: Throwable) {
+                Toast.makeText(this@Registro_Maquinaria_Activity,"Error al encontrar fabricas",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(
+                call: Call<List<FabricaDataCollectionItem>>,
+                response: Response<List<FabricaDataCollectionItem>>
+            ) {
+                for (fabrica in response.body()!!){
+                    fabricas.add("${fabrica.fabricaId}")
+                }
+
+                val adapterFabricas = ArrayAdapter(this@Registro_Maquinaria_Activity, android.R.layout.simple_spinner_item, fabricas)
+                spMaquiFarbicaID.adapter = adapterFabricas
+
+
+
+                Toast.makeText(this@Registro_Maquinaria_Activity,"Fabricas encontradas con exito",Toast.LENGTH_SHORT).show()
+
+            }
+        })
+
+    }
+
+    //POST
+    private fun callServicePostMaquinaria() {
+        val fecha = "2021-04-10"
+        val maquinariaInfo = MaquinariaDataCollectionItem(
+            maquinaId = txtMaquinariaId.text.toString().toInt(),
+            fabricaId = spMaquiFarbicaID.selectedItem.toString().toInt(),
+            marca = txtNombreMarca.text.toString(),
+            horasUso = txtHorasUsoMaqui.text.toString().toDouble(),
+            tipoMaquina = txtTipoMaquina.text.toString()
+        )
+
+        addMaquinaria(maquinariaInfo) {
+            if (it?.maquinaId != null) {
+                Toast.makeText(this@Registro_Maquinaria_Activity,"Maquina registrada"+it?.maquinaId,Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this@Registro_Maquinaria_Activity,"Error",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun addMaquinaria(maquinariaData: MaquinariaDataCollectionItem, onResult: (MaquinariaDataCollectionItem?) -> Unit){
+        val retrofit = RestEngine.buildService().create(MaquinariaService::class.java)
+        var result: Call<MaquinariaDataCollectionItem> = retrofit.addMaquinaria(maquinariaData)
+
+        result.enqueue(object : Callback<MaquinariaDataCollectionItem> {
+            override fun onFailure(call: Call<MaquinariaDataCollectionItem>, t: Throwable) {
+                onResult(null)
+            }
+
+            override fun onResponse(call: Call<MaquinariaDataCollectionItem>,
+                                    response: Response<MaquinariaDataCollectionItem>) {
+                if (response.isSuccessful) {
+                    val addedMaquinaria = response.body()!!
+                    onResult(addedMaquinaria)
+                }
+                else if (response.code() == 401){
+                    Toast.makeText(this@Registro_Maquinaria_Activity,"Sesion expirada",Toast.LENGTH_LONG).show()
+                }
+                else if (response.code() == 500){
+                    //val gson = Gson()
+                    //val type = object : TypeToken<RestApiError>() {}.type
+                    //var errorResponse1: RestApiError? = gson.fromJson(response.errorBody()!!.charStream(), type)
+                    val errorResponse = Gson().fromJson(response.errorBody()!!.string()!!, RestApiError::class.java)
+
+                    Toast.makeText(this@Registro_Maquinaria_Activity,errorResponse.errorDetails,Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this@Registro_Maquinaria_Activity,"Fallo al traer el item",Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+        )
+    }
+
+    //PUT
+
+    private fun callServicePutMaquinaria() {
+        val fecha = "2021-04-11"
+        val personInfo = MaquinariaDataCollectionItem(
+            maquinaId = txtMaquinariaId.text.toString().toInt(),
+            fabricaId = spMaquiFarbicaID.selectedItem.toString().toInt(),
+            marca = txtNombreMarca.text.toString(),
+            horasUso = txtHorasUsoMaqui.text.toString().toDouble(),
+            tipoMaquina = txtTipoMaquina.text.toString()
+        )
+
+        val retrofit = RestEngine.buildService().create(MaquinariaService::class.java)
+        var result: Call<MaquinariaDataCollectionItem> = retrofit.updateMaquinaria(personInfo)
+
+        result.enqueue(object : Callback<MaquinariaDataCollectionItem> {
+            override fun onFailure(call: Call<MaquinariaDataCollectionItem>, t: Throwable) {
+                Toast.makeText(this@Registro_Maquinaria_Activity,"Error",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(call: Call<MaquinariaDataCollectionItem>,
+                                    response: Response<MaquinariaDataCollectionItem>) {
+                if (response.isSuccessful) {
+                    val updatedPerson = response.body()!!
+                    Toast.makeText(this@Registro_Maquinaria_Activity,"Actualizado"+response.body()!!.marca,Toast.LENGTH_LONG).show()
+                }
+                else if (response.code() == 401){
+                    Toast.makeText(this@Registro_Maquinaria_Activity,"Sesion expirada",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this@Registro_Maquinaria_Activity,"Fallo al traer el item",Toast.LENGTH_LONG).show()
+                }
+            }
+
+        })
+    }
+
+    //GET
+    private fun callServiceGetMaquinaria() {
+        val maquinariaService:MaquinariaService = RestEngine.buildService().create(MaquinariaService::class.java)
+        var result: Call<MaquinariaDataCollectionItem> = maquinariaService.getMaquinariaById(txtMaquinariaId.text.toString().toLong())
+
+        result.enqueue(object : Callback<MaquinariaDataCollectionItem> {
+            override fun onFailure(call: Call<MaquinariaDataCollectionItem>, t: Throwable) {
+                Toast.makeText(this@Registro_Maquinaria_Activity,"No se encontro la maquinaria", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                call: Call<MaquinariaDataCollectionItem>,
+                response: Response<MaquinariaDataCollectionItem>
+            ) {
+                txtMaquinariaId.setText(response.body()!!.maquinaId.toString())
+                txtNombreMarca.setText(response.body()!!.marca)
+                txtHorasUsoMaqui.setText(response.body()!!.horasUso.toString())
+                txtTipoMaquina.setText(response.body()!!.tipoMaquina)
+                Toast.makeText(this@Registro_Maquinaria_Activity,"Se encontró la maquina con Id"+response.body()!!.maquinaId, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+
+    //GETS
+    private fun callServiceGetMaquinarias() {
+        val maquinariaService:MaquinariaService = RestEngine.buildService().create(MaquinariaService::class.java)
+        var result: Call<List<MaquinariaDataCollectionItem>> = maquinariaService.listMaquinarias()
+
+        result.enqueue(object :  Callback<List<MaquinariaDataCollectionItem>> {
+            override fun onFailure(call: Call<List<MaquinariaDataCollectionItem>>, t: Throwable) {
+                Toast.makeText(this@Registro_Maquinaria_Activity,"Error al traer las maquinarias",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(
+                call: Call<List<MaquinariaDataCollectionItem>>,
+                response: Response<List<MaquinariaDataCollectionItem>>
+            ) {
+                Toast.makeText(this@Registro_Maquinaria_Activity,"Maquinarias encontradas",Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    //-----
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_contextual, menu)
